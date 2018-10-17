e4f9343f5d0b5596a194fe2b9aab97d16406169e
Abayomi <abayomismart@gmail.com>
2018-10-17 16:08:55 +0100

    updated with model(class object) properly formated.

--- api_maker.php
+++ api_maker.php
@@ -30,6 +30,34 @@ class apimaker
         self::spool_api($tables);
     }
 
+    private static function table_structure($tb, $dbo){
+        $tbl = $tb["Tables_in_".$dbo->dbname];
+        $table_properties = $dbo->tableDesc($tbl);
+        return $table_properties;
+    }
+
+    private static function do_public_prop($prop){
+        $pub = "";
+        if(sizeof($prop)>0){
+            $pub = "        Object(class) public properties.\r\n"."        */ \r\n";
+            foreach($prop as $ppp){
+                $pub .= "        public $".$ppp.";\r\n";
+            }
+        }
+        return $pub;
+    }
+
+    private static function do_private_prop($prop){
+        $pri = "";
+        if(sizeof($prop)>0){
+            $pri = "        /** Object(class) private properties.*/ \r\n";
+            foreach($prop as $ppp){
+                $pri .= "        private $".$ppp.";\r\n";
+            }
+        }
+        return $pri;
+    }
+
     private static function make_readme($tables, $dbsettings){
         $readme = " HTTP RESTful API ".$dbsettings["db"]." Readme file.\n";
         $readme .= "==========================================================\n\n";
@@ -72,7 +100,7 @@ class apimaker
         utilities::xcopy('server/error', self::$base_dir.'/logs/error');
     }
 
-    private static function create_class($tables){
+    private static function makemodel($tables){
         foreach ($tables as $tb){
             if ( self::exempted($tb) === false ){
                 self::do_php_class($tb);
@@ -99,7 +127,7 @@ class apimaker
     private static function do_php_class($table){
         $dbf = self::dataObj();
         $tbl = $table["Tables_in_".$dbf->dbname];
-        $phpcode = "<?php\r\n";
+        $phpcode = "<?php\r\n\r\n\r\n";
         $phpcode .= self::get_class_docstring();
         $phpcode .= "\r\n".self::get_class_properties($table);
         $phpcode .= "\r\n\r\n".self::get_class_constructor();
@@ -122,11 +150,11 @@ class apimaker
     }
 
     private static function get_class_docstring(){
-        $docstr = '/**'."\r\n";
-        $docstr .= 'This php script implements '."\r\n\r\n";
-        $docstr .= 'PHP Version 5+'."\r\n";
-        $docstr .= '@Author: Abayomi Apetu'."\r\n";
-        $docstr .= '*/'."\r\n\r\n";
+        $docstr = '    /**'."\r\n";
+        $docstr .= '    This php script implements '."\r\n\r\n";
+        $docstr .= '    PHP Version 5+'."\r\n";
+        $docstr .= '    @Author: Abayomi Apetu'."\r\n";
+        $docstr .= '    */'."\r\n\r\n";
         return $docstr;
     }
 
@@ -141,11 +169,10 @@ class apimaker
             $nclass = $table["Tables_in_".$dbo->dbname];
         }
 
-        $defc = 'require "DbHandlers.php";'."\r\n\r\n";
-        //$defc .= 'require "dbconfig.php";'."\r\n\r\n";
-        $defc .= 'class '.ucwords($nclass).'{'."\r\n\r\n";
+        $defc = '    require "DbHandlers.php";'."\r\n\r\n";
+        $defc .= '    class '.ucwords($nclass).'{'."\r\n\r\n";
         $tprop = self::table_structure($table, $dbo);
-        $defc .= '    /** '."\r\n".'Object(class) properties.'."\r\n";
+        $defc .= '        /** '."\r\n".'        Object(class) properties.'."\r\n";
         foreach ($tprop as $tbrow) {
             if ($tbrow['Default'] !== "NULL"){
                 array_push($publ, $tbrow['Field']);
@@ -159,9 +186,9 @@ class apimaker
     }
 
     private static function get_class_constructor(){
-        $ccon = "    public function _construct(){\r\n";
-        $ccon .= "        /** Todo, add code for system initialization here!*/ \r\n";
-        $ccon.= "    }";
+        $ccon = "        public function __construct(){\r\n";
+        $ccon .= "           /** Todo, add code for system initialization here!*/ \r\n";
+        $ccon.= "        }";
         return $ccon;
     }
 
@@ -169,64 +196,63 @@ class apimaker
         $dbc = self::dataObj();
         $tbn = $table["Tables_in_".$dbc->dbname];
         $tpr = self::table_structure($table, $dbc);
-        $savestr ="    public function create(){\r\n";
-        $savestr.='        $db = new DbHandlers();'."\r\n";
-        $savestr.='        $sql = "INSERT INTO '.$tbn.'(";'."\r\n";
+        $savestr ="        public function create(){\r\n";
+        $savestr.='            $db = new DbHandlers();'."\r\n";
+        $savestr.='            $sql = "INSERT INTO '.$tbn.'(";'."\r\n";
         $itr = 0;
         foreach($tpr as $field){
-            $savestr.='        if (isset($this->'.$field['Field'].') && $this->'.$field['Field'].'!=="" ) {'."\r\n";
+            $savestr.='            if (isset($this->'.$field['Field'].') && $this->'.$field['Field'].'!=="" ) {'."\r\n";
             if ($itr == 0 ){
-            $savestr.='             $sql.= '."'".$field['Field']."';";
+            $savestr.='                $sql.= '."'".$field['Field']."';";
             } else {
                 $savestr.='            $sql.= '."',".$field['Field']."';";
             }
             $itr += 1;
-            $savestr .="    \r\n        }\r\n";
+            $savestr .="    \r\n            }\r\n";
         }
-        $savestr.='        $sql.= ") VALUES (";'."\r\n";
+        $savestr.='            $sql.= ") VALUES (";'."\r\n";
         $tr = 0;
         foreach($tpr as $fld){
-            $savestr.='        if (isset($this->'.$fld['Field']. ') && $this->'.$fld['Field'].'!=="") {'."\r\n";
+            $savestr.='            if (isset($this->'.$fld['Field']. ') && $this->'.$fld['Field'].'!=="") {'."\r\n";
             if ($tr == 0 ){
                 if ($fld['Type'] == "date" || $fld['Type'] == "datetime") {
                     if ($fld['Type'] == "date")
-                        $savestr.='            $sql.="'."'".'".'.'substr($this->'.$fld['Field'].",0,10)".'."'."'".'"'.";";
+                        $savestr.='                $sql.="'."'".'".'.'substr($this->'.$fld['Field'].",0,10)".'."'."'".'"'.";";
                     else
-                        $savestr.='            $sql.="'."'".'".'.'str_replace(".000Z", "", str_replace("T", " ",$this->'.$fld['Field']."))".'."'."'".'"'.";";
+                        $savestr.='                $sql.="'."'".'".'.'str_replace(".000Z", "", str_replace("T", " ",$this->'.$fld['Field']."))".'."'."'".'"'.";";
                 } else {
-                    $savestr.='            $sql.="'."'{".'$this->'.$fld['Field']."}'".'"'.";";
+                    $savestr.='                $sql.="'."'{".'$this->'.$fld['Field']."}'".'"'.";";
                 }
             } else {
                 if ($fld['Type'] == "date" || $fld['Type'] == "datetime") {
                     if ($fld['Type'] == "date")
-                        $savestr.='            $sql.="'.",'".'".'.'substr($this->'.$fld['Field'].",0,10)".'."'."'".'"'.";";
+                        $savestr.='                $sql.="'.",'".'".'.'substr($this->'.$fld['Field'].",0,10)".'."'."'".'"'.";";
                     else
-                        $savestr.='            $sql.="'.",'".'".'.'str_replace(".000Z", "", str_replace("T", " ", $this->'.$fld['Field']."))".'."'."'".'"'.";";
+                        $savestr.='                $sql.="'.",'".'".'.'str_replace(".000Z", "", str_replace("T", " ", $this->'.$fld['Field']."))".'."'."'".'"'.";";
                 } else {
-                    $savestr.='            $sql.="'.",'{".'$this->'.$fld['Field']."}'".'"'.";";
+                    $savestr.='                $sql.="'.",'{".'$this->'.$fld['Field']."}'".'"'.";";
                 }
             }
             $tr += 1;
-            $savestr .="    \r\n        }\r\n";
+            $savestr .="    \r\n            }\r\n";
         }
-        $savestr.='        $sql.=")";'."\r\n";
-        $savestr.='        $sql = str_replace("(,", "(", $sql);'."\r\n";
-        $savestr.='        $savein = $db->executeQuery($sql);'."\r\n";
-        $savestr.='        return $savein;'."\r\n";
-        $savestr.="    }";
+        $savestr.='            $sql.=")";'."\r\n";
+        $savestr.='            $sql = str_replace("(,", "(", $sql);'."\r\n";
+        $savestr.='            $savein = $db->executeQuery($sql);'."\r\n";
+        $savestr.='            return $savein;'."\r\n";
+        $savestr.="        }";
         return $savestr;
     }
 
     private static function get_Class_method_readAll($tbv){
         $dbv = self::dataObj();
         $tb = $tbv["Tables_in_".$dbv->dbname];
-        //$tpv = self::table_structure($tbv, $dbv);
-        $viewstr = '    public function readAll() {'."\r\n";
-        $viewstr .= '        $db = new DbHandlers();'."\r\n";
-        $viewstr .= '        $sql = "SELECT * from '.$tb.' order by id DESC";'."\r\n;        }";
-        $viewstr .= '        $datasource = $db->getRowAssoc($sql);'."\r\n";
-        $viewstr .= '        return $datasource;'."\r\n";
-        $viewstr .= '    }';
+        $viewstr = '        public function readAll() {'."\r\n";
+        $viewstr .= '           $db = new DbHandlers();'."\r\n";
+        $viewstr .= '           $sql = "SELECT * from '.$tb.' order by id DESC";'."\r\n";
+        $viewstr .= '           $datasource = $db->getRowAssoc($sql);'."\r\n";
+        $viewstr .= '           return $datasource;'."\r\n";
+        $viewstr .= '        }';
         return $viewstr;
     }
 
@@ -234,12 +260,12 @@ class apimaker
         $dbv = self::dataObj();
         $tb = $tbv["Tables_in_".$dbv->dbname];
         //$tpv = self::table_structure($tbv, $dbv);
-        $viewstr = '    public function readOne($critcol=null, $critval=null) {'."\r\n";
-        $viewstr .= '        $db = new DbHandlers();'."\r\n";
-        $viewstr .= '        $sql = "SELECT * from '.$tb.' WHERE $critcol ='."'{".'$critval'."}'".'";'."\r\n";
-        $viewstr .= '        $datasource = $db->getRowAssoc($sql);'."\r\n";
-        $viewstr .= '        return $datasource;'."\r\n";
-        $viewstr .= '    }';
+        $viewstr = '        public function readOne($critcol=null, $critval=null) {'."\r\n";
+        $viewstr .= '           $db = new DbHandlers();'."\r\n";
+        $viewstr .= '           $sql = "SELECT * from '.$tb.' WHERE $critcol ='."'{".'$critval'."}'".'";'."\r\n";
+        $viewstr .= '           $datasource = $db->getRowAssoc($sql);'."\r\n";
+        $viewstr .= '           return $datasource;'."\r\n";
+        $viewstr .= '        }';
         return $viewstr;
     }
 
@@ -247,53 +273,53 @@ class apimaker
         $dbu = self::dataObj();
         $tbn = $tbu["Tables_in_".$dbu->dbname];
         $tpr = self::table_structure($tbu, $dbu);
-        $updatestr ='    public function update($pvcol, $pval){'."\r\n";
-        $updatestr.='        $db = new DbHandlers();'."\r\n";
-        $updatestr.='        $sql = "UPDATE '.$tbn.' SET ";'."\r\n";
+        $updatestr ='        public function update($pvcol, $pval){'."\r\n";
+        $updatestr.='            $db = new DbHandlers();'."\r\n";
+        $updatestr.='            $sql = "UPDATE '.$tbn.' SET ";'."\r\n";
         $itr = 0;
         foreach($tpr as $field){
-            $updatestr.='        if (isset($this->'.$field['Field'].') && $this->'.$field['Field'].'!=="" ) {'."\r\n";
+            $updatestr.='            if (isset($this->'.$field['Field'].') && $this->'.$field['Field'].'!=="" ) {'."\r\n";
             if ($itr == 0 ){
                 if ($field['Type'] == "date" || $field['Type'] == "datetime"){
                     if ($field['Type'] == "date")
-                        $updatestr.='             $sql.= " '.$field['Field'].' = '."'".'".'.'substr($this->'.$field['Field'].", 0, 10)".'."'."'".'"'.";";
+                        $updatestr.='                 $sql.= " '.$field['Field'].' = '."'".'".'.'substr($this->'.$field['Field'].", 0, 10)".'."'."'".'"'.";";
                     else
-                        $updatestr.='             $sql.= " '.$field['Field'].' = '."'".'".'.'str_replace(".000Z", "", str_replace("T", " ", $this->'.$field['Field']."))".'."'."'".'"'.";";
+                        $updatestr.='                 $sql.= " '.$field['Field'].' = '."'".'".'.'str_replace(".000Z", "", str_replace("T", " ", $this->'.$field['Field']."))".'."'."'".'"'.";";
                 } else {
-                    $updatestr.='             $sql.= " '.$field['Field'].' = '."'{".'$this->'.$field['Field']."}'".'"'.";";
+                    $updatestr.='                 $sql.= " '.$field['Field'].' = '."'{".'$this->'.$field['Field']."}'".'"'.";";
                 }
             } else {
                 if ($field['Type'] == "date" || $field['Type'] == "datetime"){
                     if ($field['Type'] == "date")
-                        $updatestr.='            $sql.= ", '.$field['Field'].' = '."'".'".'.'substr($this->'.$field['Field'].", 0, 10)".'."'."'".'"'.";";
+                        $updatestr.='                $sql.= ", '.$field['Field'].' = '."'".'".'.'substr($this->'.$field['Field'].", 0, 10)".'."'."'".'"'.";";
                     else
-                        $updatestr.='            $sql.= ", '.$field['Field'].' = '."'".'".'.'str_replace(".000Z", "", str_replace("T", " ", $this->'.$field['Field']."))".'."'."'".'"'.";";
+                        $updatestr.='                $sql.= ", '.$field['Field'].' = '."'".'".'.'str_replace(".000Z", "", str_replace("T", " ", $this->'.$field['Field']."))".'."'."'".'"'.";";
                 } else {
-                    $updatestr.='            $sql.= ", '.$field['Field'].' = '."'{".'$this->'.$field['Field']."}'".'"'.";";
+                    $updatestr.='                $sql.= ", '.$field['Field'].' = '."'{".'$this->'.$field['Field']."}'".'"'.";";
                 }
             }
             $itr += 1;
-            $updatestr .="    \r\n        }\r\n";
+            $updatestr .="    \r\n            }\r\n";
         }
-        $updatestr.='        $sql .=  " WHERE $pvcol = '."'".'$pval'."'".'";'."\r\n";
-        $updatestr.='        $sql = str_replace("SET ,", "SET ", $sql);'."\r\n";
-        $updatestr.='        $upd = $db->executeQuery($sql);'."\r\n";
-        $updatestr.='        return $upd;'."\r\n";
-        $updatestr.="    }";
+        $updatestr.='            $sql .=  " WHERE $pvcol = '."'".'$pval'."'".'";'."\r\n";
+        $updatestr.='            $sql = str_replace("SET ,", "SET ", $sql);'."\r\n";
+        $updatestr.='            $upd = $db->executeQuery($sql);'."\r\n";
+        $updatestr.='            return $upd;'."\r\n";
+        $updatestr.="        }";
         return $updatestr;
     }
 
     private static function get_class_method_delete($tbs){
         $dbd = self::dataObj();
         $tb = $tbs["Tables_in_".$dbd->dbname];
-        $delstr = '    public  function delete($critcol, $critval){'."\r\n";
-        $delstr .='        $db = new DbHandlers();'."\r\n";
-        $delstr .='        $sql = "DELETE FROM '.$tb.' WHERE $critcol ='."'{".'$critval'."}'".'";'."\r\n";
-        $delstr .='        $d_out = $db->executeQuery($sql);'."\r\n";
-        $delstr .='        return $d_out;'."\r\n";
-        $delstr .='    }';
+        $delstr = '        public  function delete($critcol, $critval){'."\r\n";
+        $delstr .='            $db = new DbHandlers();'."\r\n";
+        $delstr .='            $sql = "DELETE FROM '.$tb.' WHERE $critcol ='."'{".'$critval'."}'".'";'."\r\n";
+        $delstr .='            $d_out = $db->executeQuery($sql);'."\r\n";
+        $delstr .='            return $d_out;'."\r\n";
+        $delstr .='        }';
         return $delstr;
     }
-    
+
     private static function spool_api($tbs){}
 }
